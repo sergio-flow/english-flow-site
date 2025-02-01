72cce5eaa501e023f524039e3196801801e58c93
sergio-flow <sergio.finciuc@gmail.com>
2025-02-01 17:27:22 +0200

    Re-deploy

--- src/app/[countryCode]/[languageCode]/page.tsx
+++ src/app/[countryCode]/[languageCode]/page.tsx
@@ -1,6 +1,5 @@
 import FilterPhrases from "@/_components/templates/FilterPhrases";
 import fetchPhrases from "@/_utils/fetchPhrases";
-import Image from "next/image";
 
 export default async function Home() {
   const phrases = await fetchPhrases();
